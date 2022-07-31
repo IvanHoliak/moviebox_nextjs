7fbf6dcca7ee450125341f6eea1db9e1bbd1aee7
IvanHoliak <goliakivan@gmail.com>
2022-07-31 16:11:18 +0300

    Updata slider content

--- components/Cards/MovieContent.tsx
+++ components/Cards/MovieContent.tsx
@@ -1,25 +1,41 @@
 import Image from "next/image";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { genres } from "../../libs/genres";
 import { IMovie } from "../../types";
 
 import styles from "./Cards.module.scss";
 
 const MovieContent: FC<IMovie> = ({adult, backdrop_path, genre_ids, id, original_language, original_title, overview, popularity, poster_path, release_date, title, video, vote_average, vote_count}) => {
-    genre_ids = genre_ids.map((id) => {
+    const [isLoaded, setIsLoaded] = useState<boolean>(false);
+
+    genre_ids = genre_ids?.map((id) => {
         return genres.filter((genres) => genres.id === id)[0].name;
     });
+
     return (
-        <>
-            <Image
-                priority
-                loader={() => `https://image.tmdb.org/t/p/w500${poster_path}`}
-                src={poster_path}
-                alt="Movie image"
-                width="250px"
-                height="370px"
-                className={styles.card_wrapper__image}
-            />
+        <div className={isLoaded ? "" : styles.card_wrapper__skeleton}>
+            {
+                poster_path ? (
+                    <Image
+                        loader={() => `https://image.tmdb.org/t/p/w500${poster_path}`}
+                        src={poster_path}
+                        alt="Actor image"
+                        width="250px"
+                        height="370px"
+                        className={styles.card_wrapper__image}
+                        onLoadingComplete={() => setIsLoaded(true)}
+                    />
+                ) : (
+                    <Image
+                        src={"/assets/img/default_user.jpg"}
+                        alt="Actor image"
+                        width="250px"
+                        height="370px"
+                        className={styles.card_wrapper__image}
+                        onLoadingComplete={() => setIsLoaded(true)}
+                    />
+                )
+            }
             <div className={styles.card_wrapper__body}>
                 <p className={styles.card_wrapper__body_release_date}>{release_date}</p>
                 <h3 className={styles.card_wrapper__body_title}>{title}</h3>
@@ -31,11 +47,11 @@ const MovieContent: FC<IMovie> = ({adult, backdrop_path, genre_ids, id, original
                         width="35"
                         height="17"
                     />
-                    <span>{vote_average * 10}.0 / 100</span>
+                    <span>{vote_average || 1 * 10}.0 / 100</span>
                 </div>
-                <p className={styles.card_wrapper__body_genres}>{genre_ids.join(", ")}</p>
+                <p className={styles.card_wrapper__body_genres}>{genre_ids?.join(", ")}</p>
             </div>
-        </>
+        </div>
     );
 };
 
