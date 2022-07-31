7c8c51acdd741382349d171f63b4b23bab442bd5
IvanHoliak <goliakivan@gmail.com>
2022-07-31 16:10:36 +0300

    Updata card content

--- components/Cards/ActorContent.tsx
+++ components/Cards/ActorContent.tsx
@@ -1,27 +1,42 @@
 import Image from "next/image";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { IActor } from "../../types";
 
 import styles from "./Cards.module.scss";
 
 const ActorContent: FC<IActor> = ({adult, gender, id, known_for, known_for_department, name, popularity, profile_path}) => {
+    const [isLoaded, setIsLoaded] = useState<boolean>(false);
+
     return (
-        <>
-            <Image
-                priority
-                loader={() => `https://image.tmdb.org/t/p/w500${profile_path}`}
-                src={profile_path}
-                alt="Movie image"
-                width="250px"
-                height="370px"
-                className={styles.card_wrapper__image}
-            />
+        <div className={isLoaded ? "" : styles.card_wrapper__skeleton}>
+            {
+                profile_path ? (
+                    <Image
+                        loader={() => `https://image.tmdb.org/t/p/w500${profile_path}`}
+                        src={profile_path}
+                        alt="Actor image"
+                        width="250px"
+                        height="370px"
+                        className={styles.card_wrapper__image}
+                        onLoadingComplete={() => setIsLoaded(true)}
+                    />
+                ) : (
+                    <Image
+                        src={"/assets/img/default_user.jpg"}
+                        alt="Actor image"
+                        width="250px"
+                        height="370px"
+                        className={styles.card_wrapper__image}
+                        onLoadingComplete={() => setIsLoaded(true)}
+                    />
+                )
+            }
             <div className={styles.card_wrapper__body}>
                 <h3 className={styles.card_wrapper__body_name}>
                     {name}
                 </h3>
             </div>
-        </>
+        </div>
     );
 };
 
--- components/Cards/CardContent.tsx
+++ components/Cards/CardContent.tsx
@@ -1,26 +1,15 @@
-import { FC } from "react";
+import React, { FC, useState } from "react";
 import { IActor, ICardContent, IMovie } from "../../types";
 import ActorContent from "./ActorContent";
 import styles from "./Cards.module.scss";
 import MovieContent from "./MovieContent";
 
 const CardContent: FC<ICardContent<IMovie, IActor>> = ({ data, type }) => {
-    const typeContent = () => {
-        switch(type){
-            case "movies": 
-                const moviesData = (data as IMovie);
-                return <MovieContent {...moviesData}/>
-            case "actors":
-                const actorData = (data as IActor);
-                return <ActorContent {...actorData}/>
-            default:
-                return null;
-        };
-    };
     return (
         <div className={styles.card}>
             <div className={styles.card_wrapper}>
-                {typeContent()}
+                {type === "movies" && <MovieContent {...(data as IMovie)}/>}
+                {type === "actors" && <ActorContent {...(data as IActor)}/>}
             </div>
         </div>
     );
